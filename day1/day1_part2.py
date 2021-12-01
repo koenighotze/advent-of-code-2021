6b5a2c0b3e4f702c619758d4377674614bb29cb8
David Schmitz <davidschmitz@posteo.de>
2021-12-01 14:57:14 +0100

    Day 1 python ci

--- day1/day1_part2.py
+++ day1/day1_part2.py
@@ -6,15 +6,15 @@ def day1(measures):
 
     number_of_increases = 0
     for i in range(len(measures) - window_size):
-        if (sum(window(measures, i + 1)) > sum(window(measures, i))):
+        if sum(window(measures, i + 1)) > sum(window(measures, i)):
             number_of_increases += 1
-            
+
     return number_of_increases
 
 def load_data(file_name):
-    file = open(file_name, 'r')
-    return list(map(lambda l: int(l.strip()), file.readlines()))
+    with open(file = file_name, mode = 'r', encoding = 'UTF-8') as file:
+        return list(map(lambda l: int(l.strip()), file.readlines()))
 
 if __name__ == '__main__':
-    number_of_increases = day1(load_data(sys.argv[1]))
-    print(f"Number of increases {number_of_increases}")
+    RESULT = day1(load_data(sys.argv[1]))
+    print(f"Number of increases {RESULT}")
--- day1/test_day1_part2.py
+++ day1/test_day1_part2.py
@@ -2,9 +2,10 @@ import unittest
 
 from day1_part2 import day1
 
+
 class TestSum(unittest.TestCase):
     def test_day1(self):
-        measures = [ 199, 200, 208, 210, 200, 207, 240, 269, 260, 263 ]
+        measures = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263]
 
         result = day1(measures)
 
@@ -12,8 +13,9 @@ class TestSum(unittest.TestCase):
 
     def test_empty_list_results_in_zero(self):
         result = day1([])
-        
+
         self.assertEqual(result, 0)
+
 
 if __name__ == '__main__':
     unittest.main()
