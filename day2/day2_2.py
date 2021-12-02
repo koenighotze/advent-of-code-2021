3042f3dc70286e8023238fc331e45464fbf6c9f3
David Schmitz <davidschmitz@posteo.de>
2021-12-02 09:56:41 +0100

    Fix coday findings

--- day2/day2_2.py
+++ day2/day2_2.py
@@ -9,14 +9,14 @@ def day2(input):
     }
 
     current = [0, 0, 0]
-    for [op, val] in [l.split() for l in input]:
+    for [op, val] in [line.split() for line in input]:
         current = commands[op](int(val), current)
     return dict(H=current[0], D=current[1])
 
 
 def load_data(file_name):
     with open(file=file_name, mode="r", encoding="UTF-8") as file:
-        return list(map(lambda l: l.strip(), file.readlines()))
+        return list(map(lambda line: line.strip(), file.readlines()))
 
 
 if __name__ == "__main__":
