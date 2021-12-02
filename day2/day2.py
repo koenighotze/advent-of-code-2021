badd946b65297ad1f3475e7f2d75268f14ead408
David Schmitz <davidschmitz@posteo.de>
2021-12-02 09:45:21 +0100

    Day2

---
+++ day2/day2.py
@@ -0,0 +1,24 @@
+import sys
+
+
+def day2(input):
+    commands = {
+        'forward': lambda v, c: [c[0] + v, c[1]],
+        'down': lambda v, c: [c[0], c[1] + v],
+        'up': lambda v, c: [c[0], c[1] - v],
+    }
+
+    current = [0, 0]
+    for [op, val] in [l.split() for l in input]:
+        current = commands[op](int(val), current)
+    return dict(H=current[0], D=current[1])
+
+
+def load_data(file_name):
+    with open(file=file_name, mode="r", encoding="UTF-8") as file:
+        return list(map(lambda l: l.strip(), file.readlines()))
+
+
+if __name__ == "__main__":
+    RESULT = day2(load_data(sys.argv[1]))
+    print(f"Result {RESULT['H'] * RESULT['D']}")
---
+++ day2/test_day2_2.py
@@ -0,0 +1,24 @@
+import unittest
+
+from day2_2 import day2
+
+
+class TestSum(unittest.TestCase):
+    def test_day2(self):
+        input = [
+            "forward 5",
+            "down 5",
+            "forward 8",
+            "up 3",
+            "down 8",
+            "forward 2"
+        ]
+
+        result = day2(input)
+
+        self.assertEqual(result['H'], 15)
+        self.assertEqual(result['D'], 60)
+
+
+if __name__ == "__main__":
+    unittest.main()
